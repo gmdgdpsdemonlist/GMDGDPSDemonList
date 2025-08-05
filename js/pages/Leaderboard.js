22487be4895cd55e8d15f9cb5bce957d61d27e66
gmdgdpsdemonlist <sashapelex1@gmail.com>
2025-08-05 09:17:42 +0200

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -100,7 +100,7 @@ export default {
                   <p>#{{ score.rank }}</p>
                 </td>
                 <td class="level">
-                  <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
+                  <p class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</p>
                 </td>
                 <td class="score">
                   <p>+{{ localize(score.score) }}</p>
