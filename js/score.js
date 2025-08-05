abe03369b1ceb611629385bdc97794d52502394c
gmdgdpsdemonlist <sashapelex1@gmail.com>
2025-08-05 08:48:02 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -37,18 +37,5 @@ export function score(rank, percent, minPercent) {
 }
 
 export function round(num) {
-    if (!('' + num).includes('e')) {
-        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
-    } else {
-        var arr = ('' + num).split('e');
-        var sig = '';
-        if (+arr[1] + scale > 0) {
-            sig = '+';
-        }
-        return +(
-            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
-            'e-' +
-            scale
-        );
-    }
+    return Math.floor(num).toLocaleString('fr-FR'); // space-separated thousands
 }
